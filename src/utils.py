4976afd3795e0014322ee03342924486e16bf080
tossan <tossanmusic@gmail.com>
2022-01-04 21:54:45 +0100

    chore: encapsulate io

--- src/utils.py
+++ src/utils.py
@@ -2,19 +2,84 @@ import numpy as np
 import pandas as pd
 import pandas_datareader.data as reader
 import datetime as dt
-from os.path import exists
+import os
 
+symbols = {
+    "stocks": {
+        "symbols": ["MFED.PA", "PE500.PA", "EN.PA", "CNP.PA", "TTE.PA"],
+        "path": "../data/stocks.csv"
+    },
+    "cryptos": {
+        "symbols": ['BTC-EUR', 'ETH-EUR', 'DOGE-EUR', 'ADA-EUR', 'SHIB-EUR'],
+        "path": "../data/cryptos.csv"
+    },
+    "cac40": {
+        "symbols": [
+            "AC.PA",
+            "ACA.PA",
+            "AI.PA",
+            "AIR.PA",
+            "ALO.PA",
+            "ATO.PA",
+            "BN.PA",
+            "BNP.PA",
+            "CA.PA",
+            "CAP.PA",
 
-def get_data(path, stocks, fetch=False):
+            "CS.PA",
+            "DG.PA",
+            "DSY.PA",
+            "EN.PA",
+            "ENGI.PA",
+            "GLE.PA",
+            "HO.PA",
+            "KER.PA",
+            "LR.PA",
+            "MC.PA",
+
+            "ML.PA",
+            "OR.PA",
+            "ORA.PA",
+            "PUB.PA",
+            "RI.PA",
+            "RMS.PA",
+            "RNO.PA",
+            "SAF.PA",
+            "SAN.PA",
+            "SGO.PA",
+
+            "STLA.PA",
+            "STM.PA",
+            "SU.PA",
+            "SW.PA",
+            "TEP.PA",
+            "TTE.PA",
+            "URW.AS",
+            "VIE.PA",
+            "VIV.PA",
+            "WLN.PA",
+        ],
+        "path": "../data/cac40.csv"
+    },
+}
+
+def get_data(kind, fetch=False, columns=["Adj Close"]):
+    if kind not in symbols.keys():
+        raise KeyError(f'{kind} is not a valid kind. try {"".join(symbols.keys())}')
+
     end = dt.datetime.now()
     start = dt.date(end.year - 5, end.month, end.day)
+    path = os.path.join(os.path.dirname(__file__), symbols[kind]["path"])
 
-    if fetch or not exists(path):
-        df = reader.get_data_yahoo(stocks, start, end)["Adj Close"].dropna()
+    if fetch or not os.path.exists(path):
+        df = reader.get_data_yahoo(symbols[kind]["symbols"], start, end)
         df.to_csv(path)
     else:
-        df = pd.read_csv(path, parse_dates=[0], index_col=0)
-    return df
+        df = pd.read_csv(path, parse_dates=[0], index_col=0, header=[0, 1])
+
+    if len(columns) == 1:
+        return df[columns[0]].dropna(), symbols[kind]["symbols"]
+    return df[columns].dropna(), symbols[kind]["symbols"]
 
 
 def get_return_simulation(df, stocks):
